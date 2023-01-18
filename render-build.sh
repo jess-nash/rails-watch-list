18cce99fc90e5a9f61bb9db0c70d317adfa67bcc
Jessica Nash <jessicanashacc@gmail.com>
2023-01-18 17:33:49 +0900

    revert

--- render-build.sh
+++ render-build.sh
@@ -5,5 +5,5 @@ set -o errexit
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
-bundle exec rake db:migrate
-bundle exec rake db:seed
+bundle exec rails db:migrate
+bundle exec rails db:seed
