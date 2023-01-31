b09abc8e1f978dd5af47003d4ebc54b724d5e5f1
Jessica Nash <jessicanashacc@gmail.com>
2023-01-31 20:48:17 +0900

    change theme to black and red

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,7 +16,7 @@ Movie.destroy_all
 # List.destroy_all
 
 # the Le Wagon copy of the API
-url = 'http://tmdb.lewagon.com/movie/top_rated'
+url = 'http://tmdb.lewagon.com/movie/now_playing'
 response = JSON.parse(URI.open(url).read)
 
 response['results'].each do |movie_hash|
